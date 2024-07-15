6ff565e66054a1be3e0f787a4753c3ba517c0276
DavidShim-Personal <huisubshm875@gmail.com>
2024-07-15 13:51:59 +0900

    1. Change formatting 2. Divide work into 3 parts to allocate among members.

--- main.py
+++ main.py
@@ -30,26 +30,34 @@ def move_one_date():
 
 
 ##############################################
+
+
 def train_data(startDate, endDate):
-    # directory에서 해당 날짜의 엑셀 파일 존재하는지 확인
-    # OpenAI API를 통해서 학습
+    # 1. directory에서 해당 날짜의 엑셀 파일 존재하는지 확인
+    # 2. OpenAI에 전달할 Prompt 작성
+    # 3. API 호출하여 학습
+    return
+
+def make_single_qt(filename, date):
+    # 1. OpenAI API를 통해 말씀의 실재 1일치 생성
+    # 2. 제목, 날짜, 내용 등을 Word 문서 형태에 맞게 저장 (filename 파일에 concat)
+    return
+
+def make_multiple_qt(cnt):
+    # 1. filename 워드 파일 생성
+    # 2. cnt만큼 make_single_qt 함수 호출, 날짜 전달
+    # 3. filename 워드 파일 최종 저장
     return
 
 
-def test_data(cnt):
-    # cnt만큼 샘플 생성
-    # OpenAI API를 통해서 chatbot한테 말 걸기
-    # cnt횟수만큼 반복
-    # word 파일에 저장
+def automate_qt():
+    # 1. 다음달 날짜 계산하기
+    # 2. make_multiple_qt에 날짜 전달하여 파일 만들기
+    # 3. 메일로 출판사, 본인 등 관련자들에게 자동 메일 발송
     return
 
 
-def make_monthly_qt(date_cnt):
-    # OpenAI api로 하나씩 받아오는 함수
-    # n회 반복해서 word에 저장
-    # 위 함수와 동작은 동일하고, 자동화를 추가한다 (특정 날짜에 시작 + 메일로 보내기)
-    return
-
+# git/github 기본
 # pull: repository -> local
 # commit: 작업한 걸 -> local (작업 단위로 commit) #comment
 # # push: local -> Repository (하루 단위로 push 한다)
