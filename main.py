edb00d03eafcd500b2935d7de1afeb86c3ff032f
DHCHO-pt <a01029595621@gmail.com>
2024-07-18 17:01:08 +0900

    save to excel

--- main.py
+++ main.py
@@ -22,6 +22,14 @@ def parse_one_page():  # 한휘
  # 동혁
 
 def save_to_excel(array_of_strings, file_name):
+    """
+    주어진 문자열 배열을 엑셀 파일에 저장합니다.
+    기존 파일이 존재하면 데이터를 추가하고, 존재하지 않으면 새로운 파일을 생성합니다.
+    
+    Parameters:
+    array_of_strings (list): 저장할 문자열 배열
+    file_name (str): 저장할 엑셀 파일 이름
+    """
     # 새로운 데이터 프레임 생성
     new_df = pd.DataFrame([array_of_strings])
     
@@ -45,13 +53,15 @@ file_name = "output.xlsx"
 save_to_excel(array_of_strings1, file_name)
 save_to_excel(array_of_strings2, file_name)
 
-def save_to_excel(data, file_name='output.xlsx', sheet_name='Sheet1'):
-    df = pd.DataFrame([data])
-    df.to_excel(file_name, index=False, header=False, sheet_name=sheet_name)
-    
-    data = ["이름", "나이", "직업"]
-    save_to_excel(data, 'example.xlsx')
-
+# move_one_date 함수의 주석과 코드를 문자열로 변환
+move_one_date_code = """
+def move_one_date():
+    # 1. 뒤로 가기
+    # 2. 다음 날짜 찾기
+    # 2-1. 다음이 10 이하면 article 누르기
+    # 2-2. 10초과면: 다음페이지 넘어가기 버튼 누르고, 현재 page는 1번으로 초기화
+    return
+"""
 
 def move_one_date():
     # 1. 뒤로 가기
